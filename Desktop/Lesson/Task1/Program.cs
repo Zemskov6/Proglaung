c4c415f70c988dcf9f84df61ade228e1329e708b
Zemskov6 <zemskovivan2005@gmail.com>
2024-02-09 22:08:28 +1000

    Добавил другие переменные

--- Desktop/Lesson/Task1/Program.cs
+++ Desktop/Lesson/Task1/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("hello");
+﻿//Console.WriteLine("hello");
+int a = 10;
+Console.WriteLine(a);
+double d =5.3;
+char c = 'a';
+bool b = true;
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
